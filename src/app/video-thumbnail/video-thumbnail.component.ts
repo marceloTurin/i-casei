a19756a20644e187f644c68aa4898805a42959f3
marcelo sousa <marcelo.sousa1997@hotmail.com>
2019-09-22 11:49:52 -0300

    Passando dados para o componente

--- src/app/video-thumbnail/video-thumbnail.component.ts
+++ src/app/video-thumbnail/video-thumbnail.component.ts
@@ -1,13 +1,15 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
     selector: 'video-thumbnail',
     templateUrl: 'video-thumbnail.component.html'
 })
 export class VideoThumbnail {
-    url = '"https://www.animenew.com.br/wp-content/uploads/2018/05/Kimetsu-no-Yaiba-est%C3%BAdio-ufotable.jpg';
-    tituloVideo = 'Titulo Video';
-    nomeCanal = 'Nome Canal';
-    descricaoVideo = 'Descricao muito bacana do video';
+
+    //Passando dados na forma declarativa do componente
+    @Input() url = '';
+    @Input() tituloVideo = '';
+    @Input() nomeCanal = '';
+    @Input() descricaoVideo = '';
 
 }
