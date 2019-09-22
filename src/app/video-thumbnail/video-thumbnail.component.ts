a7abc19fda721605caf80b74a09504b7eec6261d
marcelo sousa <marcelo.sousa1997@hotmail.com>
2019-09-22 11:24:46 -0300

    Criando primeiro componente

---
+++ src/app/video-thumbnail/video-thumbnail.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'video-thumbnail',
+    templateUrl: 'video-thumbnail.component.html'
+})
+export class VideoThumbnail {
+    url = '"https://www.animenew.com.br/wp-content/uploads/2018/05/Kimetsu-no-Yaiba-est%C3%BAdio-ufotable.jpg';
+    tituloVideo = 'Titulo Video';
+    nomeCanal = 'Nome Canal';
+    descricaoVideo = 'Descricao muito bacana do video';
+
+}
