1577e66f089082e53945e4d6506b88fbf272c916
marcelo sousa <marcelo.sousa1997@hotmail.com>
2019-09-22 13:57:50 -0300

    Importando o commomModule para os componentes

--- src/app/videos-thumbnail/videos-thumbanail.module.ts
+++ src/app/videos-thumbnail/videos-thumbanail.module.ts
@@ -1,14 +1,19 @@
 import {NgModule} from '@angular/core';
+import { CommonModule } from '@angular/common'; // Importa as diretivas do angular
 
 import { VideoThumbnailComponent } from './video-thumbnail/video-thumbnail.component';
 import { ListaVideosThumbnailComponent } from './lista-videos-thumbnail/lista-videos-thumbnail.component';
+
 
 @NgModule({
     declarations: [
         VideoThumbnailComponent,
         ListaVideosThumbnailComponent
     ],
-    exports: [VideoThumbnailComponent]
+    exports: [VideoThumbnailComponent],
+    imports:[
+        CommonModule
+    ]
 })
 export class VideosThumbnailModule {
 
